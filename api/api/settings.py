5387cd9614ffde0093d01cf9197f518c72276c49
Darren ha <nberserk@gmail.com>
2017-03-28 22:47:34 +0900

    fix configuration error

--- api/api/settings.py
+++ api/api/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = BASE_DIR + '/static'
 STATIC_URL = '/static/'
